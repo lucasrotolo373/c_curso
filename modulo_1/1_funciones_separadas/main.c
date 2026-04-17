a660fdd0ca89cebe5474913d28c52e4c902c4023
Lucas Rotolo <lucasrotolo@Lucass-MacBook-Pro.local>
2026-04-17 15:29:29 -0300

    feat: funciones en archivos separados con Makefile

---
+++ modulo_1/1_funciones_separadas/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "operaciones.h"
+
+int main() {
+    printf("%d\n", sumar(5, 3));
+    printf("%d\n", restar(5, 3));
+    return 0;
+}
---
+++ modulo_1/1_funciones_separadas/operaciones.h
@@ -0,0 +1,2 @@
+int sumar(int a, int b);
+int restar(int a, int b);
